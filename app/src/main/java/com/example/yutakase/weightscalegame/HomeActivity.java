90309e6e1f5f111f733bb56c782bc60e4ba07f3f
abantam <abantam.9@gmail.com>
2016-08-01 21:27:19 +0900

    画像をタップするとMyPageActivityに遷移するように実装

--- app/src/main/java/com/example/yutakase/weightscalegame/HomeActivity.java
+++ app/src/main/java/com/example/yutakase/weightscalegame/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.example.yutakase.weightscalegame;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +12,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.nifty.cloud.mb.core.NCMB;
 import com.nifty.cloud.mb.core.NCMBObject;
 import com.nifty.cloud.mb.core.NCMBUser;
@@ -27,26 +34,35 @@ public class HomeActivity extends AppCompatActivity {
     private Button rightButton;
 
     private String userName;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final Context c = this;
 
         //ユーザー名の取得
         NCMBUser ncmb = NCMBUser.getCurrentUser();
         userName = ncmb.getUserName();
 
-        userNameView = (TextView)findViewById(R.id.userText);
-        userNameView.setText(userName+"さんようこそ");
+        userNameView = (TextView) findViewById(R.id.userText);
+        userNameView.setText(userName + "さんようこそ");
 
         //画像の読み込み
-        image = (ImageView)findViewById(R.id.avator);
+        image = (ImageView) findViewById(R.id.avator);
         image.setImageResource(R.drawable.chara1);
 
-        Button leftButton = (Button)findViewById(R.id.leftButton);
-        Button centerButton = (Button)findViewById(R.id.centerButton);
-        Button rightButton = (Button)findViewById(R.id.rightButton);
+        Button leftButton = (Button) findViewById(R.id.leftButton);
+        Button centerButton = (Button) findViewById(R.id.centerButton);
+        Button rightButton = (Button) findViewById(R.id.rightButton);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     //ユーザ名を取得
@@ -54,4 +70,9 @@ public class HomeActivity extends AppCompatActivity {
         return "hogehoge";
     }
 
+    //アバターをクリックするとMyPageActivityに飛びます
+    public void ImageClick(View v) {
+        Intent intent = new Intent(this, MyPageActivity.class);
+        startActivity(intent);
+    }
 }
