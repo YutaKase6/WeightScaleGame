b44b5ae200c23272f75e3c1b5afed9383999f8fe
abantam <abantam.9@gmail.com>
2016-08-02 00:46:25 +0900

    画像をタップするとMyPageActivityに遷移するように実装

--- app/src/main/java/com/example/yutakase/weightscalegame/HomeActivity.java
+++ app/src/main/java/com/example/yutakase/weightscalegame/HomeActivity.java
@@ -1,26 +1,14 @@
 package com.example.yutakase.weightscalegame;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.nifty.cloud.mb.core.NCMB;
-import com.nifty.cloud.mb.core.NCMBObject;
 import com.nifty.cloud.mb.core.NCMBUser;
-
-import org.json.JSONObject;
 
 /**
  * ホーム画面
@@ -34,17 +22,11 @@ public class HomeActivity extends AppCompatActivity {
     private Button rightButton;
 
     private String userName;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        final Context c = this;
 
         //ユーザー名の取得
         NCMBUser ncmb = NCMBUser.getCurrentUser();
@@ -60,9 +42,7 @@ public class HomeActivity extends AppCompatActivity {
         Button leftButton = (Button) findViewById(R.id.leftButton);
         Button centerButton = (Button) findViewById(R.id.centerButton);
         Button rightButton = (Button) findViewById(R.id.rightButton);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 
     //ユーザ名を取得
