8eeb78609dc45a959324821a69ca348a11c1a2d8
abantam <abantam.9@gmail.com>
2016-08-03 18:17:33 +0900

    ホーム画面の背景貼り付け（暫定）

--- app/src/main/java/com/example/yutakase/weightscalegame/HomeActivity.java
+++ app/src/main/java/com/example/yutakase/weightscalegame/HomeActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -40,12 +41,14 @@ public class HomeActivity extends AppCompatActivity {
         userNameView = (TextView) findViewById(R.id.userText);
         userNameView.setText(userName + "さんようこそ");
 
+        Display disp = getWindowManager().getDefaultDisplay();
+
         //背景の読み込み
         background = (ImageView) findViewById(R.id.background);
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background);
-//        Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap, 200, 45, false);
-//        background.setImageBitmap(bitmap2);
-        background.setImageResource(R.drawable.background);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+        Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap, disp.getWidth(), disp.getHeight(), false);
+        background.setImageBitmap(bitmap2);
+//        background.setImageResource(R.drawable.background);
 
         //画像の読み込み
         image = (ImageView) findViewById(R.id.avator);
