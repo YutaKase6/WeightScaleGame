ff7e93a5a2016c4f9cd2de0d42adddf4ff83abcd
yukinasss <mayoseko@gmail.com>
2016-08-04 19:35:46 +0900

    画像差し替えました

--- app/src/main/java/com/example/yutakase/weightscalegame/StartActivity.java
+++ app/src/main/java/com/example/yutakase/weightscalegame/StartActivity.java
@@ -1,13 +1,16 @@
 package com.example.yutakase.weightscalegame;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.yutakase.weightscalegame.utils.TypefaceUtil;
 import com.nifty.cloud.mb.core.NCMB;
 import com.nifty.cloud.mb.core.NCMBUser;
 
+import butterknife.Bind;
 import butterknife.BindString;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,13 +22,14 @@ public class StartActivity extends AppCompatActivity {
     @BindString(R.string.NCMB_client_key)
     String CLIENT_KEY;
 
+    @Bind(R.id.continue_text)
+    TextView hoge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         ButterKnife.bind(this);
-
-        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "assets/keifont.ttf"); // font from assets: "assets/fonts/Roboto-Regular.ttf
 
         // NCMB 初期設定
         NCMB.initialize(this.getApplicationContext(), this.APP_KEY, this.CLIENT_KEY);
